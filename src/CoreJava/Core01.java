74585d86969b275163b7efa058c820adf1b4d6f7
Amit Yadav <ay3106222@gmail.com>
2025-08-19 06:40:14 +0530

    add code on github

---
+++ src/CoreJava/Core01.java
@@ -0,0 +1,7 @@
+package CoreJava;
+
+public class Core01 {
+    public static void main(String[] args) {
+        //this is java Core programming language
+    }
+}
---
+++ src/Trees/BinaryTree01.java
@@ -0,0 +1,8 @@
+package Trees;
+
+public class BinaryTree01 {
+    public static void main(String[] args) {
+        //this is java Binary tree Search value
+        System.out.println("this is java Binary Search Tree Programme code");
+    }
+}
