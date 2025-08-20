7fe9659a442e4669f638ddc56464088eff735d4f
Amit Yadav <ay3106222@gmail.com>
2025-08-20 21:30:03 +0530

    print java sum number programme between 1 to 100

---
+++ src/DSA/Programme002.java
@@ -0,0 +1,12 @@
+package DSA;
+
+public class Programme002 {
+    public static void main(String[] args) {
+        //Create java programme and print to 1 to 100 between sum  number
+        for (int i = 1; i <=100 ; i++) {
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
