0ff44d050bc5965e4ba4b3af5ef50476eb0ba79d
Amit Yadav <ay3106222@gmail.com>
2025-08-20 21:25:12 +0530

    create java programme and print to between 1 to 100 odd numbers

---
+++ src/DSA/Programme001.java
@@ -0,0 +1,14 @@
+package DSA;
+
+import java.util.Scanner;
+
+public class Programme001 {
+    public static void main(String[] args) {
+        //Create Odd number between to 1 to 100
+        for (int i = 1; i <=100; i++) {
+            if (i%2==1){
+                System.out.println(i);
+            }
+        }
+    }
+}
