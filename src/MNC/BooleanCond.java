4099737aa7465267cd7e5f4c3c4149aef7675b63
Amit Yadav <ay3106222@gmail.com>
2025-08-26 22:15:52 +0530

    create conditional programming using boolean value

---
+++ src/MNC/BooleanCond.java
@@ -0,0 +1,14 @@
+package MNC;
+
+public class BooleanCond {
+    public static void main(String[] args) {
+        //Create java programme for boolean conditional
+        boolean programmer=false;
+
+        if (programmer){
+            System.out.println("This is java programmer");
+        }else {
+            System.out.println("Not you are a java programmer");
+        }
+    }
+}
