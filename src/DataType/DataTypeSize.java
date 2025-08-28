b5b31a90c19c1a7287075f7d6dd704bcc9486013
Amit Yadav <ay3106222@gmail.com>
2025-08-28 21:38:59 +0530

    print all data type and size for practice

---
+++ src/DataType/DataTypeSize.java
@@ -0,0 +1,14 @@
+package DataType;
+
+public class DataTypeSize {
+    public static void main(String[] args) {
+        //print all data type size range
+        System.out.println("byte: "+Byte.MIN_VALUE+" to "+Byte.MAX_VALUE);
+        System.out.println("Short: "+Short.MIN_VALUE+" to "+Short.MAX_VALUE);
+        System.out.println("Integer: "+Integer.MIN_VALUE+" to  "+Integer.MAX_VALUE);
+        System.out.println("Long: "+Long.MIN_VALUE+" to "+Long.MAX_VALUE);
+        System.out.println("float: "+Float.MIN_VALUE+" to "+Float.MAX_VALUE);
+        System.out.println("double: "+Double.MIN_VALUE+" to "+Double.MAX_VALUE);
+        System.out.println("Character: "+Character.MIN_VALUE+" to "+Character.MAX_VALUE);
+    }
+}
