aba48169e268571b7fc404e2c676d3515511d44f
Amit Yadav <ay3106222@gmail.com>
2025-08-28 21:31:39 +0530

    print all data type for practice

---
+++ src/DataType/AllDataType.java
@@ -0,0 +1,25 @@
+package DataType;
+
+public class AllDataType {
+    public static void main(String[] args) {
+        //Print All Data Type
+        byte b=100;
+        short sr=2000;
+        int i=2001224;
+        long l=545454878l;
+        float f=235.245f;
+        double d=454.5454d;
+        char c='A';
+        boolean bool=true;
+
+        //print all data type
+        System.out.println("byte : "+b);
+        System.out.println("short : "+sr);
+        System.out.println("int : "+i);
+        System.out.println("long : "+l);
+        System.out.println("float : "+f);
+        System.out.println("double : "+d);
+        System.out.println("Char : "+c);
+        System.out.println("Boolean : "+bool);
+    }
+}
