f842b9432c1d50fc6c8af48390d5b527e05c438f
Amit Yadav <ay3106222@gmail.com>
2025-08-28 21:43:54 +0530

    convert data one data type to another data type is called Type Casting Data type

---
+++ src/DataType/TypeCasting.java
@@ -0,0 +1,11 @@
+package DataType;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        //Java type Casting
+        int num=2485;
+        System.out.println("Before Type casting: "+num);
+        double d=num;
+        System.out.println("After Type casting: "+d);
+    }
+}
